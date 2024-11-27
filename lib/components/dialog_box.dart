c5963600a2d2ac6c9894fa1f5de67ea3a24dd6be
nomispal <naumansaeedpal@gmail.com>
2024-11-27 22:51:18 +0500

    Added AlertDialog

---
+++ lib/components/dialog_box.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DialogBox extends StatelessWidget {
+  const DialogBox ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.orange[200],
+      content: Container(
+        height: 120,
+        child: Column(
+          children: [
+            TextField(),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
